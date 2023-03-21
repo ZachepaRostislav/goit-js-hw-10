78f1f13bb0919cf1a9b3685c6d798fa60f076878
ZachepaRostislav <zachepa.rostislav@gmail.com>
2023-03-21 19:12:57 +0200

    fix error

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,12 @@ export default function fetchCountries(name) {
   const URL = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 
   return fetch(URL)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error("Oops, there is no country with that name");
+      }
+      return response.json()
+    })
 }
 
 
